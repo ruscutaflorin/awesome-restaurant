1d80808713fd985a9f8cbe91f9efb932eb3ddc00
Florin Ruscuta <ruscutaf@gmail.com>
2024-03-22 06:52:36 +0200

    center the dashboard component

--- client/src/app/ui/adminPage/dashboard.tsx
+++ client/src/app/ui/adminPage/dashboard.tsx
@@ -10,51 +10,53 @@ import RevenueLines from "./revenueLines";
 
 const AdminDashboard = () => {
   return (
-    <div className="grid-container grid-cols-4 gap-4 mx-5 mt-5">
-      {/* First Row */}
-      <div className="col-span-4 grid grid-cols-4">
-        <div className="col-span-1 grid-item">
-          <Earning actualValue={64320} oldValue={40000} />
-        </div>
-        <div className="col-span-1 grid-item">
-          <Customers actualValue={3209} oldValue={4100} />
-        </div>
-        <div className="col-span-1 grid-item">
-          <EmployeesRecruit actualValue={43} oldValue={39} />
-        </div>
-        <div className="col-span-1 grid-item">
-          <TotalOrders actualValue={5730} oldValue={7500} />
-        </div>
-      </div>
-
-      {/* Second Row */}
-      <div className="col-span-3 grid gap-10 grid-cols-3 mx-5 mt-5">
-        <div className="col-span-1 grid-item">
-          <PopularTime />
-        </div>
-        <div className="col-span-1 grid-item">
-          <PieChartComponent />
-        </div>
-        <div className="col-span-1 grid-item">
-          <PaymentPieChart />
-        </div>
-      </div>
-
-      {/* Third Row */}
-      <div className="col-span-2 grid grid-cols-2 mx-5 mt-5">
-        <div className="col-span-1 grid-item">
-          <RevenueLines />
-        </div>
-        <div className=" grid-col-span-2 grid grid-cols-1 my-auto gap-5">
+    <main className="h-full flex justify-center items-center mx-auto">
+      <div className="grid-container grid-cols-4 gap-4 ">
+        {/* First Row */}
+        <div className="col-span-4 grid grid-cols-4">
           <div className="col-span-1 grid-item">
-            <Earning actualValue={1000} oldValue={200} />
+            <Earning actualValue={64320} oldValue={40000} />
           </div>
           <div className="col-span-1 grid-item">
-            <Earning actualValue={1000} oldValue={200} />
+            <Customers actualValue={3209} oldValue={4100} />
+          </div>
+          <div className="col-span-1 grid-item">
+            <EmployeesRecruit actualValue={43} oldValue={39} />
+          </div>
+          <div className="col-span-1 grid-item">
+            <TotalOrders actualValue={5730} oldValue={7500} />
+          </div>
+        </div>
+
+        {/* Second Row */}
+        <div className="col-span-3 grid gap-10 grid-cols-3 mx-5 mt-5">
+          <div className="col-span-1 grid-item">
+            <PopularTime />
+          </div>
+          <div className="col-span-1 grid-item">
+            <PieChartComponent />
+          </div>
+          <div className="col-span-1 grid-item">
+            <PaymentPieChart />
+          </div>
+        </div>
+
+        {/* Third Row */}
+        <div className="col-span-2 grid grid-cols-2 mx-5 mt-5">
+          <div className="col-span-1 grid-item">
+            <RevenueLines />
+          </div>
+          <div className=" grid-col-span-2 grid grid-cols-1 my-auto gap-5">
+            <div className="col-span-1 grid-item">
+              <Earning actualValue={1000} oldValue={200} />
+            </div>
+            <div className="col-span-1 grid-item">
+              <Earning actualValue={1000} oldValue={200} />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </main>
   );
 };
 
